f2cc80afee26483ecf751bc963e46c84c81f3457
dashamallll@outlook.com <dashamallll@outlook.com>
2022-10-01 22:41:12 +0300

    Полностью изменили базу данных и сделали так, чтобы при выборе даты, появлялись соответствующие ей планы

--- planner4/ConnectBD.cs
+++ planner4/ConnectBD.cs
@@ -1,32 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace planner4
 {
-    public class DataBD
+    
+    public class dayModel
     {
-        public string data { get; set; }
-        public string Plan1 { get; set; }
-        public string Plan2 { get; set; }
-        public string Plan3 { get; set; }
-        public string Plan4 { get; set; }
-        public string Plan5 { get; set; }
-        public string Plan6 { get; set; }
-        public string Plan7 { get; set; }
-        public int CountOfPlan { get; set; }
-        public double Water { get; set; }
-        public double Mood { get; set; }
-        public double Sleep { get; set; }
-        public double Steps { get; set; }
-        public string Motivation { get; set; }
+        [Key]
+        public int id { get; set; }
+        public DateTime date { get; set; }
+    }
+    public class planModel
+    {
+        [Key]
+        public int id { get; set; }
+        public int rel_day_id { get; set; }
+        public string plan { get; set; }
+        public int plan_position { get; set; }
     }
     public class ConnectBD: DbContext
     {
-        public DbSet<DataBD> AppBD { get; set; }
+        public DbSet<dayModel> days { get; set; }
+        public DbSet<planModel> plans { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("workstation id=baseplaner.mssql.somee.com;packet size=4096;user id=xdashie_SQLLogin_1;pwd=juo9s5krmv;data source=baseplaner.mssql.somee.com;persist security info=False;initial catalog=baseplaner");
--- planner4/MainWindow.xaml.cs
+++ planner4/MainWindow.xaml.cs
@@ -15,11 +15,64 @@ using System.Windows.Shapes;
 
 namespace planner4
 {
+    
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Finds a Child of a given item in the visual tree. 
+        /// </summary>
+        /// <param name="parent">A direct parent of the queried item.</param>
+        /// <typeparam name="T">The type of the queried item.</typeparam>
+        /// <param name="childName">x:Name or Name of child. </param>
+        /// <returns>The first parent item that matches the submitted type parameter. 
+        /// If not matching item can be found, 
+        /// a null parent is being returned.</returns>
+        public static T FindChild<T>(DependencyObject parent, string childName)
+           where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +87,32 @@ namespace planner4
             date_text.Text = data.ToLongDateString();
             calendar_planner.Visibility = Visibility.Hidden;
             date_text.Visibility = Visibility;
+            for (int i = 1; i <= 7; i++)
+            {
+                string TBText = "plan_" + i;
+                FindChild<TextBox>(Application.Current.MainWindow, TBText).Text = "";
+                
+            }
+            using (var db = new ConnectBD())
+            {
+                var selectedDate = data;
+                var day = db.days.FirstOrDefault(x => x.date == selectedDate);
+                int? dayIndex = day != null ? day.id : null; //записан ли такой день, если нет, то записывем именно дату и сохраняем
+                if (!dayIndex.HasValue)
+                {
+                    var selectedDay = new dayModel { date = data };
+                    db.days.Add(selectedDay);
+                    db.SaveChanges();
+                    dayIndex = selectedDay.id;
+                }
+
+                foreach (var item in db.plans.Where(x=> x.rel_day_id == dayIndex))//ищем план и записываем его
+                {
+                    string TBText = "plan_" + item.plan_position;
+                    var TB = FindChild<TextBox>(Application.Current.MainWindow, TBText);
+                    TB.Text = item.plan;
+                }
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -77,15 +156,6 @@ namespace planner4
         }
         public Boolean isDataExist()
         {
-            using (var db = new ConnectBD())
-            {
-                var data = db.AppBD.FirstOrDefault(item => item.data == date_text.Text);
-                if (data != null)
-                {
-                    MessageBox.Show("Нужно удалить данные, чтобы обновить их");
-                    return true;
-                }
-            }
             return false;
         }
         private void button_load_Click(object sender, RoutedEventArgs e)
@@ -93,23 +163,32 @@ namespace planner4
             if (isDataExist()) return;
             using (var db = new ConnectBD())
             {
-                db.AppBD.Add(new DataBD()
+                var selectedDate = (DateTime)calendar_planner.SelectedDate;
+                var day = db.days.FirstOrDefault(x => x.date == selectedDate);
+                int? dayIndex = day!=null ? day.id:null;
+                if (!dayIndex.HasValue)
                 {
-                    data = date_text.Text,
-                    Plan1 = plan_1.Text,
-                    Plan2 = plan_2.Text,
-                    Plan3 = plan_3.Text,
-                    Plan4 = plan_4.Text,
-                    Plan5 = plan_5.Text,
-                    Plan6 = plan_6.Text,
-                    Plan7 = plan_7.Text,
-                    CountOfPlan = int.Parse(count_of_affairs.Text),
-                    Water = slider_water.Value,
-                    Mood = slider_mood.Value,
-                    Sleep = slider_sleep.Value,
-                    Steps = slider_steps.Value,
-                    Motivation = motivation_text.Text
-                });
+                    var selectedDay = new dayModel { date = selectedDate };
+                    db.days.Add(selectedDay);
+                    db.SaveChanges();
+                    dayIndex = selectedDay.id;
+                }
+                else
+                {
+                    db.plans.RemoveRange(db.plans.Where(x => x.rel_day_id == dayIndex));
+                    db.SaveChanges();
+                }
+                
+                
+                for (int i = 1; i <= 7; i++)
+                {
+                    string TBText = "plan_" + i;
+                    var text = FindChild<TextBox>(Application.Current.MainWindow, TBText).Text;
+                    if (text.Length>0)
+                    {
+                        db.plans.Add(new planModel { plan = text, rel_day_id = dayIndex.Value, plan_position=i});
+                    }
+                }
                 db.SaveChanges();
             }
         }
